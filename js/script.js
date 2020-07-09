4a90851c847e5dd2d6de89d60d6cce87dbab83d0
Manny <manny@holdex.io>
2020-07-09 22:36:45 +0800

    populated quotes
    added 5 quote objects to the quotes array

--- js/script.js
+++ js/script.js
@@ -11,7 +11,36 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
-
+let quotes = [
+  {
+    quote: "Victory awaits him who has everything in order, luck some people call it. Defeat is certain for him who has neglected to take necessary precautions in time; this is called bad luck.",
+    source: "Roald Amundsen",
+    citation: "Sydpolen (The South Pole)",
+    year: "1912"
+  },
+  {
+    quote: "It is not the critic who counts, not the man who points out how the strong man stumbled, or where the doer of deeds could have done better. The credit belongs to the man who is actually in the arena; whose face is marred by the dust and sweat and blood; who strives valiantly; who errs and comes short again and again, because there is no effort without error or shortcoming; who knows the great enthusiasms, the great devotions and spends himself in a worthy cause; who at the best, knows in the end the triumph of high achievement, and who, at worst, if he fails, at least fails while daring greatly; so that his place shall never be with those cold and timid souls who know neither victory or defeat.",
+    source: "Theodore Roosevelt",
+    citation: "Citizenship in a Republic",
+    year: "1910"
+  },
+  {
+    quote: "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+    source: "Winston Churchill (misatributed)",
+    citation: "a Budweiser beer ad campaign"
+  },
+  {
+    quote: "I am the one thing in life I can control.",
+    source: "Lin-Manuel Miranda's Aaron Burr",
+    citation: "Hamilton: An American Musical",
+    year: "2015"
+  },
+  {
+    quote: "Look to this day! For it is life, the very life of life. In its brief course lie all the verities and realities of your existence: the bliss of growth; the glory of action; the splendor of achievement. For yesterday is but a dream, and tomorrow is only a vision. But today, well lived, makes every yesterday a dream of happiness, and every tomorrow a vision of hope. Look well, therefore to this day! Such is the salutation to the dawn.",
+    source: "Kalidasa",
+    citation: "Salutation to the Dawn"
+  }
+];
 
 /***
  * `getRandomQuote` function
